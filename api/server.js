2d7740f80c05f5ae08b421a4ef6b5e64dec9ded3
Josh <josh@phin.co>
2019-04-13 10:48:03 -0700

    ability to serve production assets

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@
   const app = express();
   
   app.use( express.static( path.join( __dirname, '/public' ) ) );
+  app.use( express.static( path.join( __dirname, '../dist' ) ) );
   app.use( bodyParser.json() );
   app.use( bodyParser.urlencoded({ extended: true }) );
   
