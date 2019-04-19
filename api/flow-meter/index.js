a2e8bf6f8c19a85fa96b5b673c9ae144f521d4d6
Josh <josh@phin.co>
2019-04-18 23:02:24 -0700

    oops

--- api/flow-meter/index.js
+++ api/flow-meter/index.js
@@ -10,7 +10,7 @@ exports.listen = function(){
   const gpioPin = 23;
   const tapIndex = 3;// TODO: stop hardcoding these tapIndex - use config
 
-  const flowMeter = new FlowMeter( FlowMeter.gpioPin( gpioPin ) );
+  const flowMeter = new FlowMeter( FlowMeter.createGPIO( gpioPin ) );
   flowMeter.on('pour_start', exports.pourStart.bind( null, tapIndex ) ); 
   flowMeter.on('pour_end', exports.pourEnd.bind( null, tapIndex ) );
   flowMeters.push( flowMeter );
