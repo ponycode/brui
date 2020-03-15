a5bbd5a4f70cc937fce51d66e1c8913fcfabd54a
Josh <josh@phin.co>
2020-03-15 14:39:17 -0700

    wakeup works

--- api/lib/Wakeup.js
+++ api/lib/Wakeup.js
@@ -1,4 +1,5 @@
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
+const DISPLAY_0 = { DISPLAY: ':0' };
 
 class WakeUp {
 
@@ -7,14 +8,26 @@ class WakeUp {
    */
   static loudNoise(){
     try{
-      exec('xset s reset', {
-        env: {
-          DISPLAY: ':0'
-        }
+      execSync('/opt/vc/bin/tvservice -p', { // power on HDMI
+        env: DISPLAY_0
+      })
+      execSync('xset s reset', {  // Turn off screensaver
+        env: DISPLAY_0
       })
       console.log('BANG BANG BANG!');
     }catch( e ){
       console.error(`Error making loud noise: ${e}`);
+    }
+  }
+
+  static sleep(){
+    try{
+      execSync('/opt/vc/bin/tvservice -0', { // power off HDMI
+        env: DISPLAY_0
+      })
+      console.log('GO TO SLEEP!');
+    }catch( e ){
+      console.error(`Error going to sleep: ${e}`);
     }
   }
 
